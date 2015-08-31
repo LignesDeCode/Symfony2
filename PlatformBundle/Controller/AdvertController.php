66aae3671936433f78c3301ac760ed5b228fe2f2
Tom <tom.lyngen@gmail.com>
2015-08-31 10:14:30 +0200

    Slug

--- PlatformBundle/Controller/AdvertController.php
+++ PlatformBundle/Controller/AdvertController.php
@@ -9,6 +9,17 @@ class AdvertController extends Controller
 {
     public function indexAction()
     {
-        return new Response("Hello le monde !");
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Felix'));
+        return new Response($content);
+    }
+    public function viewAction($id)
+    {
+        
+        return new Response("Affichage de l'annonce d'id : ".$id);
+    }
+    public function viewSlugAction($slug,$year,$format)
+    {
+        
+        return new Response("On affiche l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format.".");
     }
 }
